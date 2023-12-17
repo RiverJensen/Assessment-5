92d397d4fb4f0f0aa556b5054d8ffbfd21407ec1
River Jensen <river@code1088.com>
2023-12-17 10:34:37 -0700

    update

--- answers.sql
+++ answers.sql
@@ -11,6 +11,27 @@ SELECT id FROM orders WHERE customer_id IN (SELECT customer_id FROM customers WH
 SELECT SUM(cupcakes) AS sum FROM orders WHERE order = 'unprocessed';
 -- QUESTION 4
 
+SELECT c.name AS name, COALESCE(SUM(o.num_cupcakes), 0) AS sum
+FROM cupcakes c
+LEFT JOIN orders o ON c.id = o.cupcake_id
+GROUP BY c.name
+ORDER BY c.name;
+
+
+
 -- QUESTION 5
 
+SELECT c.email AS email, COALESCE(SUM(o.num_cupcakes), 0) AS total
+FROM customers c
+LEFT JOIN orders o ON c.id = o.customer_id
+GROUP BY c.email
+ORDER BY total DESC;
+
+
 -- QUESTION 6
+
+SELECT DISTINCT c.fname, c.lname, c.email
+FROM customers c
+JOIN orders o ON c.id = o.customer_id
+JOIN cupcakes cp ON o.cupcake_id = cp.id
+WHERE cp.name = 'funfetti' AND o.processed = TRUE;
--- src/model.js
+++ src/model.js
@@ -1,8 +1,8 @@
-import { DataTypes, Model } from 'sequelize';
-import util from 'util';
-import connectToDB from './db.js';
+import { DataTypes, Model } from "sequelize";
+import util from "util";
+import connectToDB from "./db.js";
 
-const db = await connectToDB('postgresql:///animals');
+const db = await connectToDB("postgresql:///animals");
 
 export class Human extends Model {
   [util.inspect.custom]() {
@@ -10,58 +10,68 @@ export class Human extends Model {
   }
 
   getFullName() {
-    return `${this.fname} ${this.lname}`
+    return `${this.fname} ${this.lname}`;
   }
 }
 
-Human.init({
-  human_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
+Human.init(
+  {
+    human_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    fname: {
+      type: DataTypes.TEXT(50),
+      allowNull: false,
+    },
+    lname: {
+      type: DataTypes.TEXT(50),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.TEXT(100),
+      allowNull: false,
+    },
   },
-  fname:{
-    type: DataTypes.TEXT(50),
-    allowNull: false,
-  },
-  lname: {
-    type: DataTypes.TEXT(50),
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.TEXT(100),
-    allowNull: false,
+  {
+    sequelize: db,
+    modelName: "Human",
   }
-})
+);
 
 export class Animal extends Model {
   [util.inspect.custom]() {
     return this.toJSON();
-
   }
 }
 
-Animal.init({
-  animal_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: false,
+Animal.init(
+  {
+    animal_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.TEXT(50),
+      allowNull: false,
+    },
+    species: {
+      type: DataTypes.TEXT(50),
+      allowNull: false,
+    },
+    birth_year: {
+      type: DataTypes.INTEGER,
+    },
   },
-  name:{
-    type: DataTypes.TEXT(50),
-    allowNull:false
-  },
-  species:{
-    type: DataTypes.TEXT(50),
-    allowNull: false,
-  },
-  birth_year:{
-    type: DataTypes.INTEGER
+  {
+    sequelize: db,
+    modelName: "Animal",
   }
+);
 
-})
-
-Human.hasMany(Animal, { foreignKey: 'human_id' });
-Animal.belongsTo(Human, { foreignKey: 'human_id' });
+Human.hasMany(Animal, { foreignKey: "human_id" });
+Animal.belongsTo(Human, { foreignKey: "human_id" });
 
 export default db;
